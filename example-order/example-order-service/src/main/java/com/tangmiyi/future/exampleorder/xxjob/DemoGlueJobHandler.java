5e5b4377f4e8a49eefe4b1c349918a21fc86b6a8
tangmyi <terrymy@qq.com>
2021-01-19 11:12:30 +0800

    xxl-job modify sample

--- example-order/example-order-service/src/main/java/com/tangmiyi/future/exampleorder/xxjob/DemoGlueJobHandler.java
+++ example-order/example-order-service/src/main/java/com/tangmiyi/future/exampleorder/xxjob/DemoGlueJobHandler.java
@@ -1,15 +1,29 @@
 package com.tangmiyi.future.exampleorder.xxjob;
 
+import com.tangmiyi.future.exampleorder.pojo.param.PropertiesSnowParam;
+import com.tangmiyi.future.exampleorder.service.PropertiesSnowService;
 import com.xxl.job.core.biz.model.ReturnT;
 import com.xxl.job.core.handler.IJobHandler;
-import lombok.extern.slf4j.Slf4j;
+import com.xxl.job.core.log.XxlJobLogger;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Slf4j
 public class DemoGlueJobHandler extends IJobHandler {
+
+    @Autowired
+    private PropertiesSnowService propertiesServiceSnow;
 
     @Override
     public ReturnT<String> execute(String param) throws Exception {
-        log.info("XXL-JOB, ok"+param);
+        XxlJobLogger.log("propertiesServiceSnow start");
+        PropertiesSnowParam propertiesSnowParam = new PropertiesSnowParam();
+        propertiesSnowParam.setKey("A");
+        propertiesSnowParam.setValue("A");
+        propertiesSnowParam.setApplication("A");
+        propertiesSnowParam.setProfile("A");
+        propertiesSnowParam.setLable("A");
+        propertiesSnowParam.setCommon(0);
+        propertiesServiceSnow.insert(propertiesSnowParam);
         return ReturnT.SUCCESS;
     }
+
 }
